375b7bfd1ed285b1dd293ef93bf9fc09554e7e85
Daniel Raniz Raneland <raniz@spiideo.com>
2017-04-30 16:09:54 +0200

    Added a crafting macro.
    If you start it on items it'll craft potions over and over and over again
    
    I have no idea what happens when it runs out of materials

--- src/main/kotlin/se/raneland/ffbe/ui/GraphSelector.kt
+++ src/main/kotlin/se/raneland/ffbe/ui/GraphSelector.kt
@@ -31,7 +31,8 @@ import javax.swing.JTextArea
 val DESCRIPTIONS = listOf(
         GraphDescription("Earth Shrine", "Runs the Earth Shrine over and over, uses lapis to refresh when out of NRG", "/machines/earth-shrine.yml"),
         GraphDescription("Earth Shrine (no lapis)", "Runs the Earth Shrine over and over, waits for energy to recover over time", "/machines/earth-shrine-no-lapis-refresh.yml"),
-        GraphDescription("Floating Continent - PRO (no lapis)", "Runs the Floating Continent - PRO over and over, waits for energy to recover over time", "/machines/the-floating-continent-pr.yml")
+        GraphDescription("Floating Continent - PRO (no lapis)", "Runs the Floating Continent - PRO over and over, waits for energy to recover over time", "/machines/the-floating-continent-pro.yml"),
+        GraphDescription("Craft", "Crafts the first item in the ATK tab over and over and over again", "/machines/craft.yml")
 )
 
 
