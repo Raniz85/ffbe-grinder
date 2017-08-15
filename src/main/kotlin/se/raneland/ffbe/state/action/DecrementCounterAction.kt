f8d2787943d867c91e559109521c279448c945df
Daniel Raniz Raneland <raniz@spiideo.com>
2017-08-15 14:13:23 +0200

    Default to add/sub 1 to/from counters

--- src/main/kotlin/se/raneland/ffbe/state/action/DecrementCounterAction.kt
+++ src/main/kotlin/se/raneland/ffbe/state/action/DecrementCounterAction.kt
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit
  * @author Raniz
  * @since 2017-05-09.
  */
-class DecrementCounterAction(val name: String, val amount: Int, repeat: Boolean): AbstractGameAction(repeat) {
+class DecrementCounterAction(val name: String, val amount: Int = 1, repeat: Boolean = false): AbstractGameAction(repeat) {
     override fun execute(controller: DeviceController, counters: MutableMap<String, Int>) {
         counters.compute(name) { k, v -> (v ?: 0) - amount }
     }
